6bb70796e56334ed9f0b6a40f03fef97eb1d3f52
Clement CHAMEYRAT <cchameyr@e2r10p19.42.fr>
2018-06-22 18:01:51 +0200

    add anti duplicate installation

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
+
+
 rm ~/.zshrc.custom
 ln -s `pwd`/.zshrc.custom ~/
-echo "source ~/.zshrc.custom" >> ~/.zshrc
+if [ "`grep -c "source ~/.zshrc.custom" ~/.zshrc`" -gt "0" ]
+	then
+		echo "source already installed ; reloading"
+	else
+		echo "install..."
+		echo "source ~/.zshrc.custom" >> ~/.zshrc
+fi
 source ~/.zshrc
 
