231d26c2e6a01f3ee34b5dbb6f9f6289629d5426
Clement CHAMEYRAT <cchameyr@e1r4p19.42.fr>
2018-09-05 10:56:57 +0200

    update gign command

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@ rm ~/.zshrc.custom
 ln -s `pwd`/.zshrc.custom ~/
 if [ "`grep -c "source ~/.zshrc.custom" ~/.zshrc`" -gt "0" ]
 	then
+		cp gitignore ~/.gitignoreSave
 		echo "source already installed ; reloading"
 	else
 		echo "install..."
+		cp gitignore ~/.gitignoreSave
 		echo "source ~/.zshrc.custom" >> ~/.zshrc
 fi
 source ~/.zshrc
-
