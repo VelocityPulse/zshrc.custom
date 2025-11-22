bfcd6b53935cbb84f3ea522fd795adb38b7b89a5
Clément Chameyrat <velocitypulse@outlook.fr>
2025-11-22 20:13:38 +0100

    Implement pure to the installation

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 rm ~/.zshrc.custom
 ln -s `pwd`/.zshrc.custom ~/
 if [ "`grep -c "source ~/.zshrc.custom" ~/.zshrc`" -gt "0" ]
@@ -9,4 +11,7 @@ if [ "`grep -c "source ~/.zshrc.custom" ~/.zshrc`" -gt "0" ]
 		cp gitignore ~/.gitignoreSave
 		echo "source ~/.zshrc.custom" >> ~/.zshrc
 fi
+
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
 source ~/.zshrc
